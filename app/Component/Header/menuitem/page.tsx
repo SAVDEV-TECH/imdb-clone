3a7a0288c17730b62f2a5ddc1d71185a246154e6
savdev <savde388@gmail.com>
2025-05-15 06:49:02 -0700

    uu

--- app/Component/Header/menuitem/page.tsx
+++ app/Component/Header/menuitem/page.tsx
@@ -1,18 +1,23 @@
- import React from 'react'
- import Link from 'next/link'
-// import { title } from 'process'
- interface item{
-  address:string,
-  title:string
- }
- const menuitem = ({address,title}: item) => {
-     
-   return (
-     <div>
-       <Link className='text-[18px] px-2 rounded-sm font-extrabold bg-[#f5c518]' href={address}>{title}</Link>
-     </div>
-   )
- }
- 
- export default menuitem
+import React from 'react';
+import Link from 'next/link';
+
+interface MenuItemProps {
+  address: string;
+  title: string;
+}
+
+const MenuItem = ({ address, title }: MenuItemProps) => {
+  return (
+    <div>
+      <Link 
+        className='text-[18px] px-2 rounded-sm font-extrabold bg-[#f5c518]' 
+        href={address}
+      >
+        {title}
+      </Link>
+    </div>
+  );
+};
+
+export default MenuItem;
  
